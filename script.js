ab9623abc64ae160d9a7dc5ef2209fe46fb00bd2
Elvis <elvostudios@hotmail.com>
2020-03-09 16:00:11 +0100

    Added basic forms for future functions

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// let key = 'my key';  // once you have a key, it is ok to store it in a variable
-// const baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + key;
-// const viewRequest = baseUrl + '&op=select';
-// fetch(url).then(???).catch(???)
+const xhr = new XMLHttpRequest;
+const url = 'https://www.forverkliga.se/JavaScript/api/crud.php?requestKey';
+xhr.responseType = 'json';
+xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+
+    }
+}
+xhr.open('GET', url);
+xhr.send();
+let key = xhr.response;  // once you have a key, it is ok to store it in a variable
+const baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + key;
+const viewRequest = baseUrl + '&op=select';
+//fetch(url).then(???).catch(???)
