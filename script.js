e832e313fd74402e024b239f3a734628e7843e54
Elvis <elvostudios@hotmail.com>
2020-03-09 16:57:53 +0100

    Added failed attempt of onclick-event

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-const xhr = new XMLHttpRequest;
-const url = 'https://www.forverkliga.se/JavaScript/api/crud.php?requestKey';
-xhr.responseType = 'json';
-xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-
-    }
-}
-xhr.open('GET', url);
-xhr.send();
-let key = xhr.response;  // once you have a key, it is ok to store it in a variable
+let key = '1i1aQ';  // once you have a key, it is ok to store it in a variable
 const baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + key;
-const viewRequest = baseUrl + '&op=select';
+//const viewRequest = baseUrl + '&op=select';
 //fetch(url).then(???).catch(???)
+
+function addBook() {
+    const bookName = document.getElementById(bookNameID).value;
+    const bookAuthor = document.getElementById(authorNameID).value;
+    const insertRequest = baseUrl + `&op=insert&${bookName}&${bookAuthor}`;
+    const xhr = new XMLHttpRequest();
+    const url = insertRequest;
+    const data = JSON.stringify({id: bookName, author: bookAuthor});
+    xhr.responseType = 'json';
+    xhronreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            console.log('Book added');
+        }
+    }
+    xhr.open('POST', url);
+    xhr.send(data);
+}
+
+const addBookButton = document.getElementById('add-book-button');
+addBookButton.onclick = addBook();
