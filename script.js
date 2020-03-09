f7e9948e2eb8cbff2bcc69dd5a1ce9da2da196b8
Elvis <elvostudios@hotmail.com>
2020-03-09 14:51:21 +0100

    Setting up basics of index head

---
+++ script.js
@@ -0,0 +1,4 @@
+// let key = 'my key';  // once you have a key, it is ok to store it in a variable
+// const baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + key;
+// const viewRequest = baseUrl + '&op=select';
+// fetch(url).then(???).catch(???)
